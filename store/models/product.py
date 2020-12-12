d34acd30b5b7dc9fb94b6ff1076781ee0012446a
MehediMK <mehedikhan.cse@gmail.com>
2020-12-12 17:42:26 +0600

    update Products

--- store/models/product.py
+++ store/models/product.py
@@ -4,7 +4,8 @@ from .category import Category
 class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField(default=0)
-    description = models.TextField(max_length=500,null=True,blank=True)
+    unit = models.CharField(max_length = 15,blank=True)
+    description = models.TextField(max_length=1000,null=True,blank=True)
     category = models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
     image = models.ImageField(upload_to = 'products/')
 
